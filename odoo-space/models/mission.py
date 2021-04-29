5bd1863b7b986a027c736479470a5618568ac6f1
John Truong <jot@odoo.com>
2021-04-29 18:17:50 +0000

    [IMP] mission graph view

--- odoo-space/models/mission.py
+++ odoo-space/models/mission.py
@@ -3,16 +3,22 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 
+
 class Mission(models.Model):
     _name = 'space.mission'
     _description = 'Mission Info'
 
     name = fields.Char(string='Name')
-    ship_id = fields.Many2one(comodel_name = 'space.spaceship',
-                             string = 'Assigned Ship')
-    
-    crew_ids = fields.Many2many(comodel_name = 'res.partner',
-                               string = 'Crew Members')
-    
-    launchDate = fields.Date(string = 'Launch Date')
-    returnDate = fields.Date(string = 'Return Date')
+    ship_id = fields.Many2one(comodel_name='space.spaceship',
+                              string='Assigned Ship')
+
+    crew_ids = fields.Many2many(comodel_name='res.partner',
+                                string='Crew Members')
+
+    launchDate = fields.Date(string='Launch Date')
+    returnDate = fields.Date(string='Return Date')
+
+    # Metrics
+    fuelUsed = fields.Float(string='Fuel Used', default=0.0)
+    numberOfEngines = fields.Integer(string='Number of Engines', default=1)
+    distanceTraveled = fields.Float(string='Distance Traveled', default=0.0)
--- odoo-space/models/spaceship.py
+++ odoo-space/models/spaceship.py
@@ -3,27 +3,32 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 
+
 class Spaceship(models.Model):
     _name = 'space.spaceship'
     _description = 'Spaceship Info'
 
     name = fields.Char(string="name")
-    shipType = fields.Selection(string='Type', selection=[
-        ('shuttle', 'Shuttle'), ('rocket', 'Rocket'), ('cargo', 'Cargo')])
+    shipType = fields.Selection(string='Type',
+                                selection=[('shuttle', 'Shuttle'),
+                                           ('rocket', 'Rocket'),
+                                           ('cargo', 'Cargo')])
     numPassengers = fields.Integer(string='Number of Passengers')
-    fuelType = fields.Selection(string='Fuel Type', selection=[(
-        'gasoline', 'Gasoline'), ('jet_propellant', 'Jet Propellant')])
+    fuelType = fields.Selection(string='Fuel Type',
+                                selection=[('gasoline', 'Gasoline'),
+                                           ('jet_propellant', 'Jet Propellant')
+                                           ])
     length = fields.Float(string='Length')
     width = fields.Float(string='Width')
     height = fields.Float(string='Height')
     active = fields.Boolean(string='Active', default=False)
 
-    mission_ids = fields.One2many(comodel_name = 'space.mission',
-                                 inverse_name = 'ship_id',
-                                 string = 'Missions')
-    
+    mission_ids = fields.One2many(comodel_name='space.mission',
+                                  inverse_name='ship_id',
+                                  string='Missions')
+
     @api.constrains('length', 'width')
     def _check_dimensions(self):
         for record in self:
-            if(record.length < record.width):
+            if (record.length < record.width):
                 raise UserError("Ship's length must be larger than its width!")
