9edb15d26eaffaa8078fb65150e49b21162f13de
John Truong <jot@odoo.com>
2021-04-29 18:30:49 +0000

    [IMP] add rental map view

--- odoo-library/__manifest__.py
+++ odoo-library/__manifest__.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Library Management",
-
-    'summary': """
+    'name':
+    "Library Management",
+    'summary':
+    """
         Manage library books and customers""",
-
-    'description': """
+    'description':
+    """
         Manage library books and customers
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author':
+    "My Company",
+    'website':
+    "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Training',
-    'version': '0.1',
+    'category':
+    'Training',
+    'version':
+    '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'web_map'],
 
     # always loaded
     'data': [
